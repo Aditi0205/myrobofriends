6a62c80fd2b2d1088cd27d1f646d91d380fdfd91
Aditi <aditisrivastava220@gmail.com>
2020-07-25 12:33:50 +0530

    Updates

---
+++ precache-manifest.b0d82cd813fc39d97694626f6388212b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e778d343b1ae179538d95122b2c636d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f653fa2cdb9bd3b4ac97",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5bd7a9b1f0ed82b17d82",
+    "url": "/static/css/main.ad169868.chunk.css"
+  },
+  {
+    "revision": "f653fa2cdb9bd3b4ac97",
+    "url": "/static/js/2.6f73cbf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f73cbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd7a9b1f0ed82b17d82",
+    "url": "/static/js/main.80e33426.chunk.js"
+  },
+  {
+    "revision": "8d9cb924a9bd87bcd7ef",
+    "url": "/static/js/runtime-main.f57b2d1e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
